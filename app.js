fafbab6f2dc7c9055959c8647c97c85e6c3fe5f3
raunaqrox <sahebjot94@gmail.com>
2016-04-10 19:00:34 +0530

    fallback polling added

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
-const io = require('socket.io')(server)
+const io = require('socket.io')(server, {transport:['websocket','polling']})
 const pug = require('pug')
 const path = require('path')
 const fs = require('fs')
 
 const port = process.env.PORT || 3000
 const socketSetup = require('./sockets')(io)
+
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug')
