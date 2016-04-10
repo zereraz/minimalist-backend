f1f72e69154b478f7dc70f6d542ab5e9f74f67a8
raunaqrox <sahebjot94@gmail.com>
2016-04-10 18:35:53 +0530

    minor changes

--- sockets/index.js
+++ sockets/index.js
@@ -1,10 +1,9 @@
 const level = require('levelup')
 const sockDb = level('./sockDb', {valueEncoding: 'json'})
 
-sockDb.put('test', 'value')
-
 exports = module.exports = (io) => {
   io.on('connection', (socket) => {
     console.log("Connected")
   })
+  return io
 }
