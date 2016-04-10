3b8b5d072587ed013c19dee6157eb2e7a0f9fdd9
raunaqrox <sahebjot94@gmail.com>
2016-04-10 15:48:04 +0530

    heroku ready

--- sockets/index.js
+++ sockets/index.js
@@ -1,3 +1,6 @@
+const level = require('levelup')
+const sockDb = level('../sockDb', {valueEncoding: 'json'})
+
 exports = module.exports = (io) => {
   io.on('connection', (socket) => {
     console.log("Connected")
