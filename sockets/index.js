a6787cbb70d828264976fe8c220149c8368b91d9
raunaqrox <sahebjot94@gmail.com>
2016-04-11 15:35:02 +0530

    new person and other events

--- sockets/index.js
+++ sockets/index.js
@@ -1,9 +1,31 @@
 const level = require('levelup')
 const sockDb = level('./sockDb', {valueEncoding: 'json'})
+const shortid = require('shortid')
 
 exports = module.exports = (io) => {
   io.on('connection', (socket) => {
-    console.log("Connected")
+    socket.on("storeLocation", (location) => {
+      var longLat = location.split(":")
+      var connId = shortid.generate()
+      var person = {
+        "id": connId,
+        "location": location
+      }
+      sockDb.put(connId, JSON.stringify(person), (err) => {
+        if(err)
+          throw new Error(err.toString())
+        socket.emit("setId", connId)
+        showNewUser(socket, person)
+      })
+    })
   })
   return io
+}
+
+function showNewUser(socket, person) {
+  socket.broadcast.emit("newUser", person)
+}
+
+function setMarker(socket, location) {
+  socket.emit("setMarker", location)
 }
